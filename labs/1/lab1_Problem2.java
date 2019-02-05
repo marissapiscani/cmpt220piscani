0328a3ae8fb378a41cc92b87cbb876247702bd00
marissapiscani <47011232+marissapiscani@users.noreply.github.com>
2019-02-05 17:42:54 -0500

    Add files via upload

---
+++ labs/1/lab1_Problem2.java
@@ -0,0 +1,17 @@
+package cmpt220;
+
+public class lab1_Problem2 {
+	
+	
+	public static void main(String[] args) {
+		double pi1 = 4*(1.0-(1.0/3)+(1.0/5)-(1.0/7)+(1.0/9)-(1.0/11));
+				
+		double pi2 = 4*(1.0-(1.0/3)+(1.0/5)-(1.0/7)+(1.0/9)-(1.0/11)+(1.0/13));
+	
+	
+		System.out.println("The first approximation of pi is:" + pi1);
+		System.out.println("The second approximation of pi is:" + pi2);
+	}
+	
+}
+
