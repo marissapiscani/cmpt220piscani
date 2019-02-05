b3df08e5af15ba29fba422ff7f79cf9dfcf27af5
marissapiscani <47011232+marissapiscani@users.noreply.github.com>
2019-02-05 17:25:25 -0500

    Add files via upload

---
+++ labs/1/lab1_Problem1.java
@@ -0,0 +1,19 @@
+package cmpt220;
+
+public class lab1_Problem1 {
+	
+	
+	
+	    public static void main(String[] args) {
+	    	
+		double equation = (15.4 * 6.5 - 4.3 * 3) / (46.3 - 3.5);
+		
+		
+		System.out.println("The reult of the equation is: " + equation);
+		
+
+	}
+
+	}
+
+	
